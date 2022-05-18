9d4cf3493c943f118169a1336f4d37239c57ebc2
Jakub Perżyło <qizot1405@gmail.com>
2022-05-18 15:45:35 +0200

    Add display name user identification

--- index.js
+++ index.js
@@ -1,37 +1,48 @@
-const functions = require('@google-cloud/functions-framework')
-const { verifySlackSignature } = require("./src/middleware/slack.js")
-const { getThreadRepliesUsers, getUsersData, sendDebtSummaryMessage } = require("./src/slack.js")
-const { fetchUsersDebts } = require("./src/splitwise.js")
-
+const functions = require("@google-cloud/functions-framework");
+const { verifySlackSignature } = require("./src/middleware/slack.js");
+const {
+  getThreadRepliesUsers,
+  getUsersData,
+  sendDebtSummaryMessage,
+} = require("./src/slack.js");
+const { fetchUsersDebts } = require("./src/splitwise.js");
 
 function bundleHandlers(handlers) {
-  return async (req, res) =>  {
-    let executeNext = true
-    
-    const runNext = () => executeNext = true
-    
+  return async (req, res) => {
+    let executeNext = true;
+
+    const runNext = () => (executeNext = true);
+
     for (const handler of handlers) {
       if (executeNext) {
-        executeNext = false
+        executeNext = false;
 
-        if (handler.constructor.name === 'AsyncFunction') {
-          await handler(req, res, runNext)
+        if (handler.constructor.name === "AsyncFunction") {
+          await handler(req, res, runNext);
         } else {
-          handler(req, res, runNext)
+          handler(req, res, runNext);
         }
-        
-        continue
-      } 
 
-      break
+        continue;
+      }
+
+      break;
     }
-  }
+  };
 }
 
-function registerCloudFunction(name, ...handlers)  {
-  functions.http(name, bundleHandlers(handlers))
+function registerCloudFunction(name, ...handlers) {
+  functions.http(name, bundleHandlers(handlers));
 }
 
+function findSplitwiseUser(splitwiseUser, slackUsers) {
+  return slackUsers.find((slackUser) => {
+    return (
+      Object.values(slackUser).includes(splitwiseUser.name) ||
+      splitwiseUser.email === slackUser.email
+    );
+  });
+}
 
 async function handleRequest(req, res) {
   const { challenge, type, event } = req.body;
@@ -44,34 +55,37 @@ async function handleRequest(req, res) {
   // we only want to react on the bot being metntioned
   if (type === "event_callback" && event.type === "app_mention") {
     const { channel, thread_ts } = event;
-    
+
     const users = await getThreadRepliesUsers(channel, thread_ts);
-    
+
     if (users === null) {
-      return res.send("No users, bye!")
+      return res.send("No users, bye!");
     }
-    const usersData = (await Promise.all(users.map(user => getUsersData(user))))
-      .filter(({email}) => email !== null && email !== undefined)
-    
-    const emails = usersData.map(({email}) => email)
-    
-    const userDebts = await fetchUsersDebts()
+    const slackUsers = (
+      await Promise.all(users.map((user) => getUsersData(user)))
+    ).filter(({ email }) => email !== null && email !== undefined);
+
+    const userDebts = await fetchUsersDebts();
     const presentUserDebts = userDebts
-      .filter(userDebt => emails.includes(userDebt.email))
+      .map((splitwiseUser) => {
+        const mappedUser = findSplitwiseUser(splitwiseUser, slackUsers);
 
-    presentUserDebts.sort((a, b) => a.debt - b.debt)
-    
-    await sendDebtSummaryMessage(channel, thread_ts, presentUserDebts.map(userDebt => {
-      const u = usersData.find(({email}) => email === userDebt.email);
+        if (mappedUser === undefined) {
+          return undefined;
+        }
 
-      return {...userDebt, name: u.name}
-    }))
-    
-    return res.status(200).send("ok")
+        return { ...splitwiseUser, name: mappedUser.display_name };
+      })
+      .filter((splitwiseUser) => splitwiseUser !== undefined);
+
+    presentUserDebts.sort((a, b) => a.debt - b.debt);
+
+    await sendDebtSummaryMessage(channel, thread_ts, presentUserDebts);
+
+    return res.status(200).send("ok");
   } else {
-    return res.status(400).send("unhandled event")
+    return res.status(400).send("unhandled event");
   }
-  
 }
-  
-registerCloudFunction("slack-handler", verifySlackSignature, handleRequest)
+
+registerCloudFunction("slack-handler", verifySlackSignature, handleRequest);
