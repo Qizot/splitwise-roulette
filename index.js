34e43a3d684c0b22f047a0b5e33293c97b93eb0d
Mariusz Morawski <marmor157pl@gmail.com>
2022-06-24 00:00:09 +0200

    Working, shit code

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const functions = require("@google-cloud/functions-framework");
 const { verifySlackSignature } = require("./src/middleware/slack.js");
 const {
-  getThreadRepliesUsers,
   getUsersData,
   sendDebtSummaryMessage,
-  getBotMentionMessageUsers,
+  getUsersFromEvent,
+  getBotId,
+  getTimeFromMessage,
+  scheduleEvent,
 } = require("./src/slack.js");
-const { fetchUsersDebts } = require("./src/splitwise.js");
+const { fetchUsersDebts, findSplitwiseUser } = require("./src/splitwise.js");
 
 function bundleHandlers(handlers) {
   return async (req, res) => {
@@ -36,68 +38,70 @@ function registerCloudFunction(name, ...handlers) {
   functions.http(name, bundleHandlers(handlers));
 }
 
-function findSplitwiseUser(splitwiseUser, slackUsers) {
-  return slackUsers.find((slackUser) => {
-    return (
-      Object.values(slackUser).includes(splitwiseUser.name) ||
-      splitwiseUser.email === slackUser.email
-    );
-  });
+async function sendDebtMessage(event) {
+  const { channel, thread_ts } = event;
+
+  const users = await getUsersFromEvent(event);
+
+  if (users.length === 0) {
+    return res.send("No users, bye!");
+  }
+
+  const slackUsers = (
+    await Promise.all(users.map((user) => getUsersData(user)))
+  ).filter(({ email }) => email !== null && email !== undefined);
+
+  const userDebts = await fetchUsersDebts();
+  const presentUserDebts = userDebts
+    .map((splitwiseUser) => {
+      const mappedUser = findSplitwiseUser(splitwiseUser, slackUsers);
+
+      if (mappedUser === undefined) {
+        return undefined;
+      }
+
+      return {
+        ...splitwiseUser,
+        name: mappedUser.real_name,
+        user_id: mappedUser.user_id,
+      };
+    })
+    .filter((splitwiseUser) => splitwiseUser !== undefined);
+
+  presentUserDebts.sort((a, b) => a.debt - b.debt);
+
+  await sendDebtSummaryMessage(channel, thread_ts, presentUserDebts);
+}
+
+async function processEvent(event) {
+  const botId = await getBotId();
+  if (botId === event.user) {
+    const oldEvent = JSON.parse(event.text);
+    return await sendDebtMessage(oldEvent);
+  }
+  const timeSet = getTimeFromMessage(event.blocks);
+
+  if (!timeSet) {
+    return await sendDebtMessage(event);
+  } else {
+    return await scheduleEvent(event, timeSet, botId);
+  }
 }
 
 async function handleRequest(req, res) {
   const { challenge, type, event } = req.body;
-  
-
 
   // required for app url validation
   if (type === "url_verification") {
     return res.send(challenge);
   }
 
-
   // we only want to react on the bot being metntioned
-  if (type === "event_callback" && (event.type === "app_mention" || event.type === "message")) {
-    const { channel, thread_ts } = event;
-    
-    let threadRepliesUsers = [];
-    let botMentionsMessageUsers = [];
-
-    // get users that have replied to the thread that the bot has been mentioned in
-    if (event.type === "app_mention") {
-      threadRepliesUsers = await getThreadRepliesUsers(channel, thread_ts)
-    }
-    // get users mentioned in the same mention as the bot
-    botMentionsMessageUsers = getBotMentionMessageUsers(event.blocks)
-    
-    const users = [...new Set([...threadRepliesUsers, ...botMentionsMessageUsers])];
-    
-    if (users.length === 0) {
-      return res.send("No users, bye!");
-    }
-
-    const slackUsers = (
-      await Promise.all(users.map((user) => getUsersData(user)))
-
-    ).filter(({ email }) => email !== null && email !== undefined);
-
-    const userDebts = await fetchUsersDebts();
-    const presentUserDebts = userDebts
-      .map((splitwiseUser) => {
-        const mappedUser = findSplitwiseUser(splitwiseUser, slackUsers);
-
-        if (mappedUser === undefined) {
-          return undefined;
-        }
-
-        return { ...splitwiseUser, name: mappedUser.real_name, user_id: mappedUser.user_id };
-      })
-      .filter((splitwiseUser) => splitwiseUser !== undefined);
-
-    presentUserDebts.sort((a, b) => a.debt - b.debt);
-
-    await sendDebtSummaryMessage(channel, thread_ts, presentUserDebts);
-
+  if (
+    type === "event_callback" &&
+    (event.type === "app_mention" || event.type === "message")
+  ) {
+    await processEvent(event);
     return res.status(200).send("ok");
   } else {
     return res.status(400).send("unhandled event");
--- src/slack.js
+++ src/slack.js
@@ -1,3 +1,4 @@
+const { parse, isBefore, addDays } = require("date-fns");
 const fetch = require("node-fetch");
 
 const BASE = "https://slack.com/api/";
@@ -34,6 +35,19 @@ async function getUsersData(userId, token = TOKEN) {
   }
 }
 
+async function getBotId(token = TOKEN) {
+  try {
+    const response = await slackRequest(token, `auth.test`);
+
+    const data = await response.json();
+    return data.user_id;
+  } catch (error) {
+    console.log(error);
+
+    return null;
+  }
+}
+
 // returns a list of user ids that took part in given thread conversation
 async function getThreadRepliesUsers(channelId, threadTs, token = TOKEN) {
   try {
@@ -52,17 +66,77 @@ async function getThreadRepliesUsers(channelId, threadTs, token = TOKEN) {
   }
 }
 
-function getBotMentionMessageUsers(blocks)  {
-    return blocks
-    .filter(({type}) => type === "rich_text")
-    .map(({elements}) => 
+function getTimeFromMessage(blocks) {
+  const hour = blocks
+    .filter(({ type }) => type === "rich_text")
+    .map(({ elements }) =>
       elements
-      .filter(({type: elementType}) => elementType === "rich_text_section")
-      .map(({elements}) => elements)
+        .filter(({ type: elementType }) => elementType === "rich_text_section")
+        .map(({ elements }) => elements)
     )
     .flat(10)
-    .filter(({type}) => type === "user")
-    .map(({user_id}) => user_id)
+    .filter(({ type }) => type === "text")
+    .map(({ text }) => {
+      return text.match(/([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]/g)?.[0];
+    })
+    .filter((hour) => hour !== undefined)[0];
+
+  if (!hour) return undefined;
+
+  let date = parse(hour, "HH:mm", new Date());
+  if (isBefore(date, new Date())) date = addDays(date, 1);
+
+  return date;
+}
+
+function getBotMentionMessageUsers(blocks) {
+  return blocks
+    .filter(({ type }) => type === "rich_text")
+    .map(({ elements }) =>
+      elements
+        .filter(({ type: elementType }) => elementType === "rich_text_section")
+        .map(({ elements }) => elements)
+    )
+    .flat(10)
+    .filter(({ type }) => type === "user")
+    .map(({ user_id }) => user_id);
+}
+
+async function getUsersFromEvent(event) {
+  const { channel, thread_ts } = event;
+  let threadRepliesUsers = [];
+
+  // get users that have replied to the thread that the bot has been mentioned in
+  if (event.type === "app_mention") {
+    threadRepliesUsers = await getThreadRepliesUsers(channel, thread_ts);
+  }
+  // get users mentioned in the same mention as the bot
+  const botMentionsMessageUsers = getBotMentionMessageUsers(event.blocks);
+
+  const users = [
+    ...new Set([...threadRepliesUsers, ...botMentionsMessageUsers]),
+  ];
+
+  return users;
+}
+
+async function scheduleEvent(event, date, botId, token = TOKEN) {
+  const unixTimestamp = Math.floor(date.getTime() / 1000);
+
+  const payload = {
+    channel: botId,
+    text: JSON.stringify(event),
+    post_at: unixTimestamp,
+  };
+
+  return await fetch(BASE + "chat.scheduleMessage", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify(payload),
+  });
 }
 
 async function sendDebtSummaryMessage(
@@ -100,9 +174,9 @@ async function sendDebtSummaryMessage(
         fields: [
           {
             type: "mrkdwn",
-            text: `*Zwycięzca (<@${topDebt.user_id}>):*\n ${topDebt.name} *${topDebt.debt.toFixed(
-              2
-            )}zł*`,
+            text: `*Zwycięzca (<@${topDebt.user_id}>):*\n ${
+              topDebt.name
+            } *${topDebt.debt.toFixed(2)}zł*`,
           },
         ],
       },
@@ -129,6 +203,10 @@ async function sendDebtSummaryMessage(
 module.exports = {
   getUsersData,
   getThreadRepliesUsers,
+  getBotId,
+  getTimeFromMessage,
   sendDebtSummaryMessage,
-  getBotMentionMessageUsers
+  getBotMentionMessageUsers,
+  getUsersFromEvent,
+  scheduleEvent,
 };
